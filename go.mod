8f58a123eb03399e276ac66e40b04dd41ae6b1df
YagoAyala <yago.ayala@outlook.com>
2021-09-17 15:20:25 -0300

    feat: created a simple validation for login

--- go.mod
+++ go.mod
@@ -34,5 +34,6 @@ require (
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- src/models/login.go
+++ src/models/login.go
@@ -1,6 +1,56 @@
 package models
 
+import (
+	"log"
+
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"gopkg.in/go-playground/validator.v9"
+	en_translations "gopkg.in/go-playground/validator.v9/translations/en"
+)
+
 type Login struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"passwd"`
+}
+
+func doValidation() {
+	translator := en.New()
+	uni := ut.New(translator, translator)
+
+	trans, found := uni.GetTranslator("en")
+	if !found {
+		log.Fatal("translator not found")
+	}
+
+	v := validator.New()
+
+	if err := en_translations.RegisterDefaultTranslations(v, trans); err != nil {
+		log.Fatal(err)
+	}
+
+	_ = v.RegisterTranslation("required", trans, func(ut ut.Translator) error {
+		return ut.Add("required", "{0} is a required field", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("required", fe.Field())
+		return t
+	})
+
+	_ = v.RegisterTranslation("email", trans, func(ut ut.Translator) error {
+		return ut.Add("email", "{0} must be a valid email", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("email", fe.Field())
+		return t
+	})
+
+	_ = v.RegisterTranslation("passwd", trans, func(ut ut.Translator) error {
+		return ut.Add("passwd", "{0} is not strong enough", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("passwd", fe.Field())
+		return t
+	})
+
+	_ = v.RegisterValidation("passwd", func(fl validator.FieldLevel) bool {
+		return len(fl.Field().String()) > 6
+	})
 }
